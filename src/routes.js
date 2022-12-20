eed44924fac5a620296855ce95e95ff16eb1253b
Kaique <kaique@neurony.com.br>
2022-12-19 21:09:14 -0300

    Adiciona header

--- src/routes.js
+++ src/routes.js
@@ -2,9 +2,12 @@
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+import Header from "./components/header/Header";
+
 function RoutesApp() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="*" element={<div>TESTE 2</div>} />
       </Routes>
